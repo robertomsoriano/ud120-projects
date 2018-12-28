e712dee0916438a4b176803323b290659a132683
Robert Soriano <44214671+robertomsoriano@users.noreply.github.com>
2018-12-28 09:43:21 -0500

    Udacity's Intro to Machine Learning Lessons
    These are my scripts for lessons 1-6 of the Intro to Machine Learning Udacity course. I will upload the files for the next lesson accordingly.

---
+++ decision_tree/dt_author_id.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 3 (decision tree) mini-project.
+
+    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+
+
+#########################################################
+### your code goes here ###
+print len(features_train[0])
+from sklearn import tree
+t0 = time()
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+print "Training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "Predicting time:", round(time()-t1, 3), "s"
+
+
+from sklearn.metrics import accuracy_score
+accuracy= accuracy_score(pred, labels_test)
+t2 = time()
+
+print "accuracy: ", accuracy
+print "Accuracy scoring time:", round(time()-t2, 3), "s"
+
+
+
+
+#########################################################
+
+
